feb0961fe0413e710d815e39a918a6c0481f08ce
Nuris Akbar <nuris.akbar@gmail.com>
2019-08-06 22:24:10 +0700

    perbaikan response

--- app/Transformers/HarvestTransformer.php
+++ app/Transformers/HarvestTransformer.php
@@ -29,23 +29,28 @@ class HarvestTransformer extends Transformer
      */
     public function transform(Harvest $harvest)
     {
-        $images = [];
+        $backImages     = [];
+        $frontImages    = [];
 
-        array_push($images,secure_asset('img_harvest/'.$harvest->image_1));
+        array_push($backImages,['image_1'=>secure_asset('img_harvest/'.$harvest->image_1)]);
+        array_push($frontImages,secure_asset('img_harvest/'.$harvest->image_1));
 
         if($harvest->image_2!='')
         {
-            array_push($images,secure_asset('img_harvest/'.$harvest->image_2));
+            array_push($backImages,['image_2'=>secure_asset('img_harvest/'.$harvest->image_2)]);
+            array_push($frontImages,secure_asset('img_harvest/'.$harvest->image_2));
         }
 
         if($harvest->image_3!='')
         {
-            array_push($images,secure_asset('img_harvest/'.$harvest->image_3));
+            array_push($backImages,['image_3'=>secure_asset('img_harvest/'.$harvest->image_3)]);
+            array_push($frontImages,secure_asset('img_harvest/'.$harvest->image_3));
         }
 
         if($harvest->image_4!='')
         {
-            array_push($images,secure_asset('img_harvest/'.$harvest->image_4));
+            array_push($backImages,['image_4'=>secure_asset('img_harvest/'.$harvest->image_4)]);
+            array_push($frontImages,secure_asset('img_harvest/'.$harvest->image_4));
         }
 
 
@@ -54,10 +59,6 @@ class HarvestTransformer extends Transformer
             'title'             =>  $harvest->title,
             'slug'              =>  $harvest->slug,
             'description'       =>  $harvest->description,
-            //'user_id'           =>  $harvest->user_id,
-            //'image'             =>  $harvest->photos()->first()['file_name'],
-            'images'            =>  $images[0],
-            //'user_name'         =>  $harvest->user->name,
             'user'          =>  ['id'=>$harvest->user->id,'name'=>$harvest->user->name,'photo'=>secure_asset('img_user/'.$harvest->user->photo)],
             'view'              =>  $harvest->view,
             'publish_date'      =>  $harvest->created_at,
@@ -65,7 +66,9 @@ class HarvestTransformer extends Transformer
             'estimated_income'  =>  $harvest->estimated_income,
             'land'              => $harvest->land,
             'category'          =>  $harvest->category->name,
-            'unit'              =>  $harvest->unit->name,
+            'unit'              =>  $harvest->unit,
+            'back_images'   => $backImages,
+            'front_images'  => $frontImages,
             'region'        => [
                 'vilage'    =>  $harvest->user->village->name,
                 'district'  =>  $harvest->user->village->district->name,
--- app/Transformers/ProductTransformer.php
+++ app/Transformers/ProductTransformer.php
@@ -30,24 +30,47 @@ class ProductTransformer extends Transformer
      */
     public function transform(Product $product)
     {
+        $backImages     = [];
+        $frontImages    = [];
+
+        array_push($backImages,['image_1'=>secure_asset('img_product/'.$product->image_1)]);
+        array_push($frontImages,secure_asset('img_product/'.$product->image_1));
+
+        if($product->image_2!='')
+        {
+            array_push($backImages,['image_2'=>secure_asset('img_product/'.$product->image_2)]);
+            array_push($frontImages,secure_asset('img_product/'.$product->image_2));
+        }
+
+        if($product->image_3!='')
+        {
+            array_push($backImages,['image_3'=>secure_asset('img_product/'.$product->image_3)]);
+            array_push($frontImages,secure_asset('img_product/'.$product->image_3));
+        }
+
+        if($product->image_4!='')
+        {
+            array_push($backImages,['image_4'=>secure_asset('img_harvest/'.$product->image_4)]);
+            array_push($frontImages,secure_asset('img_harvest/'.$product->image_4));
+        }
+
+
         return [
-            'id'            =>  (int) $product->id,
+            'id'            =>  (string) $product->id,
             'name'          =>  $product->name,
             'slug'          =>  $product->slug,
             'price'         =>  $product->price,
             'price_idr'     =>  number_format($product->price,0,',','.'),
-            // 'category_id'   =>  $product->category_id,
             'stock'         =>  $product->stock,
-            // 'category_name' =>  $product->category->name,
             'description'   =>  $product->description,
             'image_url'     =>  secure_asset('img_product/'.$product->image_1),
-            // 'user_id'       =>  $product->user_id,
-            // 'user_name'     =>  $product->user->name,
             'user'          =>  ['id'=>$product->user->id,'name'=>$product->user->name,'photo'=>secure_asset('img_user/'.$product->user->photo)],
             'category'      =>  $product->category,
             'view'          =>  $product->view,
             'weight'        =>  $product->weight,
-            'unit'          =>  $product->unit->name,
+            'unit'          =>  $product->unit,
+            'back_images'   => $backImages,
+            'front_images'  => $frontImages,
             'publish_date'  =>  date_format($product->created_at,"d/m/Y"),
            'region'        => [
                 'vilage'    =>  $product->user->village->name,
